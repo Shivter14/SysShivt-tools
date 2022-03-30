8fa26337b0938403b162e2a0962619016361998d
Shivter <98386496+Shivter14@users.noreply.github.com>
2022-03-30 09:25:36 +0200

    Update store.cmd

--- store.cmd
+++ store.cmd
@@ -1,6 +1,19 @@
 @echo off
+if "%sst.dir%" equ "" (
+	echo.This program requires SysShivt tools.
+	timeout 5 > nul
+	goto end
+)
+:start
 color 0f
 mode 96,25
 cls
-echo.Still in developement. . .
-pause
+echo.$debug=%sst.errorlevel%
+echo.  Welcome to SysShivt tools store %ls.username%.
+echo.  [still in developement]
+echo.  [BACKSPACE] / [ESC] = exit
+getinput
+set sst.errorlevel=%errorlevel%
+set errorlevel=
+goto start
+:end
