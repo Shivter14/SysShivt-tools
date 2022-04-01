dc37bec36447307095182eab7dfd02ef2b1a0025
Shivter <98386496+Shivter14@users.noreply.github.com>
2022-04-01 19:42:41 +0200

    Update update.cmd

--- update.cmd
+++ update.cmd
@@ -6,6 +6,15 @@ echo.Current version: %sst.ver%
 echo.Update version: %sst.updatever%
 echo.Downloading lates version
 call "%sst.dir%\spin.cmd" 26 7 4 0f /c
+if not exist sstoolsupdate md sstoolsupdate
+cd sstoolsupdate
+call download.exe "https://github.com/Shivter14/SysShivt-tools/raw/main/SysShivt-tools-3-0-b-0329.zip" SysShivt-tools-3-0-b-0329.zip
+if not exist "SysShivt-tools-3-0-b-0329.zip" (
+  echo.Download failed!
+  pause
+) else (
+  start .
+)
 goto end
 :uptolate
 echo.You are up to late!
