25e19aa1a94db30976f7db9a3a04101e110c20e1
Shivter <98386496+Shivter14@users.noreply.github.com>
2022-05-05 17:13:40 +0200

    Update update.cmd

--- update.cmd
+++ update.cmd
@@ -1,11 +1,15 @@
 @echo off
-set sst.updatever=3.0.0
+set sst.updatever=3.0.1
 if "%sst.ver%" equ "%sst.updatever%" goto uptolate
-echo.Update avalible! [build: 0427]
+echo.Update avalible! [build: 0505]
 echo.Current version: %sst.ver%
 echo.Update version: %sst.updatever%
 echo.Downloading lates version
-call "%sst.dir%\spin.cmd" 26 7 4 0f /c
+if exist "%sst.dir%\spin.cmd" (
+  call "%sst.dir%\spin.cmd" 26 7 4 0f /c
+ ) else (
+  call spin 26 7 4 0f /c
+ )
 if not exist sstoolsupdate md sstoolsupdate
 cd sstoolsupdate
 if exist "SysShivt-tools-3-0-0-0427.zip" del "SysShivt-tools-3-0-0-0427.zip"
@@ -18,6 +22,6 @@ if not exist "SysShivt-tools-3-0-0-0427.zip" (
 )
 goto end
 :uptolate
-echo.You are up to late! [build: 0427]
+echo.You are up to late! [build: 0505]
 pause
 :end
