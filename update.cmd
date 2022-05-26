bb952ec080fa41abd549c0d326a64f33171fc990
Shivter <98386496+Shivter14@users.noreply.github.com>
2022-05-26 20:12:39 +0200

    Update update.cmd

--- update.cmd
+++ update.cmd
@@ -1,20 +1,16 @@
 @echo off
-set sst.updatever=3.0.1
+set sst.updatever=3.0.2
 if "%sst.ver%" equ "%sst.updatever%" goto uptolate
-echo.Update avalible! [build: 0505]
+echo.Update avalible! [build: 0526]
 echo.Current version: %sst.ver%
 echo.Update version: %sst.updatever%
 echo.Downloading lates version
-if exist "%sst.dir%\spin.cmd" (
-  call "%sst.dir%\spin.cmd" 26 7 4 0f /c
- ) else (
-  call spin 26 7 4 0f /c
- )
+call spin 26 7 4 0f /c
 if not exist sstoolsupdate md sstoolsupdate
 cd sstoolsupdate
-if exist "SysShivt-tools-3-0-0-0427.zip" del "SysShivt-tools-3-0-0-0427.zip"
-call download.exe "https://github.com/Shivter14/SysShivt-tools/raw/main/SysShivt-tools-3-0-1-0505.zip" SysShivt-tools-3-0-1-0505.zip
-if not exist "SysShivt-tools-3-0-1-0505.zip" (
+if exist "SysShivt-tools-3-0-1-0526.zip" del "SysShivt-tools-3-0-1-0526.zip"
+call download.exe "https://github.com/Shivter14/SysShivt-tools/raw/main/SysShivt-tools-3-0-1-0526.zip" SysShivt-tools-3-0-1-0526.zip
+if not exist "SysShivt-tools-3-0-1-0526.zip" (
   echo.Download failed!
   pause
 ) else (
@@ -22,6 +18,6 @@ if not exist "SysShivt-tools-3-0-1-0505.zip" (
 )
 goto end
 :uptolate
-echo.You are up to late! [build: 0505]
+echo.You are up to late! [build: 0526]
 pause
 :end
