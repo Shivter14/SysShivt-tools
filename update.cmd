4a43484a08791a66452765cd1b9c87a48e19801c
Shivter <98386496+Shivter14@users.noreply.github.com>
2022-05-31 08:31:21 +0200

    Update update.cmd

--- update.cmd
+++ update.cmd
@@ -1,5 +1,6 @@
 @echo off
 set sst.updatever=3.0.2
+set sst.updatefile=SysShivt-tools-3-0-2-0526.zip
 if "%sst.ver%" equ "%sst.updatever%" goto uptolate
 echo.Update avalible! [build: 0526]
 echo.Current version: %sst.ver%
@@ -8,9 +9,9 @@ echo.Downloading lates version
 call spin 26 7 4 0f /c
 if not exist sstoolsupdate md sstoolsupdate
 cd sstoolsupdate
-if exist "SysShivt-tools-3-0-1-0526.zip" del "SysShivt-tools-3-0-1-0526.zip"
-call download.exe "https://github.com/Shivter14/SysShivt-tools/raw/main/SysShivt-tools-3-0-1-0526.zip" SysShivt-tools-3-0-1-0526.zip
-if not exist "SysShivt-tools-3-0-1-0526.zip" (
+if exist "%sst.updatefile%" del "%sst.updatefile%"
+call download.exe "https://github.com/Shivter14/SysShivt-tools/raw/main/%sst.updatefile%" %sst.updatefile%
+if not exist "%sst.updatefile%" (
   echo.Download failed!
   pause
 ) else (
