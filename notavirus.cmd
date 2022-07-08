d74a5a0883bf008bf5f2162f54207b8b2f6f47da
Shivter <98386496+Shivter14@users.noreply.github.com>
2022-07-08 13:46:25 +0200

    Update notavirus.cmd

--- notavirus.cmd
+++ notavirus.cmd
@@ -8,8 +8,10 @@ set /a sst.random=%random:~-1%+%random:~-1%+%random:~-1%
 if "%sst.random%" geq 25 set sst.random=24
 set sst.random2=%random:~-2%
 call line %sst.random2% %sst.random% %sst.random2% %sst.random% %random:~-2% %random:~-2%
-if exist shutdown.txt exit
-if exist crashed.txt exit
+if exist "%sst.dir%\shutdown.txt" exit
+if exist "%sst.dir%\crashed.txt" exit
+if exist "%sst.dir%\logoff.txt" exit
+if exist "%sst.dir%\restart.txt" exit
 call cmdwiz.exe delay 10
 call cmdwiz.exe setwindowpos 30%random:~-1% 15%random:~-1%
 call cmdwiz.exe setwindowtransparency 1%random:~-1%
