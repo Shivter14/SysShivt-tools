ecf9b36bc255c414306346534afd6849581a23c2
Shivter <98386496+Shivter14@users.noreply.github.com>
2022-08-31 11:16:51 +0200

    Update update.cmd

--- update.cmd
+++ update.cmd
@@ -1,28 +1,33 @@
 @echo off
-set sst.updatever=3.1.0
-set sst.updatefile=SysShivt-tools-3-1-0-0707.zip
+set sst.updatever=3.1.1
+set sst.updatebuild=0831
+set sst.updatefile=SysShivt-tools-3-1-1-%sst.updatebuild%.zip
+echo.
 if "%sst.ver%" equ "%sst.updatever%" goto UpToLate
 if "%sst.ver%" equ "3.1.i" goto dev
-echo.Update avalible! [build: 0526]
-echo.Current version: %sst.ver%
-echo.Update version: %sst.updatever%
-echo.Downloading lates version. . .
+for %%a in (
+  "Update avalible! [build: %sst.updatebuild%]"
+  "Current version: %sst.ver%"
+  "Update version: %sst.updatever%"
+  ""
+  "Downloading lates version. . ."
+ ) do echo.  %%~a
 timeout 1 /nobreak > nul
 if not exist sstoolsupdate md sstoolsupdate
 cd sstoolsupdate
 if exist "%sst.updatefile%" del "%sst.updatefile%"
 call download.exe "https://github.com/Shivter14/SysShivt-tools/raw/main/%sst.updatefile%" %sst.updatefile%
 if not exist "%sst.updatefile%" (
-  echo.Download failed!
-  pause
+  echo.  Download failed! Press any key to exit. . .
+  pause>nul
 ) else (
   start .
 )
 goto end
 :UpToLate
-echo.You are up to late! [build: 0707]
+echo.  You are up to late! [build: %sst.updatebuild%]
 pause
 goto end
 :dev
-echo.You are running the lates developer edition.
+echo.  You are running the lates developer edition.
 :end
