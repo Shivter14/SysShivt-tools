9055a2681a4db43fdd8d7d0dc2dc82e905d4f88d
Shivter <98386496+Shivter14@users.noreply.github.com>
2023-12-21 08:23:06 +0100

    Delete dev/sysshivt-tools-canary/systemapps directory
    Reuploading in progress

--- dev/sysshivt-tools-canary/systemapps/programfiles/shell.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-if "%sst.time%" neq "%time:~0,-6%" call batbox /c %sst.startmenu.tbfgcolor% /g %sst.startmenu.cl1% %sst.defaultresY% /d "%time:~0,-6%" /g %1 %2
-set sst.time=%time:~0,-6%
--- dev/sysshivt-tools-canary/systemapps/rtview.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-:main
-cls
-type %1
-timeout 0 /nobreak > nul
-goto main
