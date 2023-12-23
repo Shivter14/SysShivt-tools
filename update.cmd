156b33412c9f8c7bd29ada4147ec0fa89ab4310e
Shivter <98386496+Shivter14@users.noreply.github.com>
2023-12-24 00:15:04 +0100

    Update update.cmd

--- update.cmd
+++ update.cmd
@@ -1,8 +1,10 @@
 @echo off
-set sst.updatever=3.1.4
-set sst.updatebuild=1830
+set sst.updatever=3.2.0
+set sst.updatebuild=2423
 set sst.updatefile=SysShivt-tools-3-1-4-%sst.updatebuild%.zip
 set sst.updateargs=%~1
+set sst.latestdevbuild=Not avaliable
+set sst.latestcanarybuild=Not avaliable
 echo.
 if %sst.build% gtr %sst.updatebuild% goto dev
 if "%sst.build%" equ "%sst.updatebuild%" goto UpToLate
@@ -25,6 +27,11 @@ set sst.update=True
 goto end
 :UpToLate
 echo.  You are up to date. [build: %sst.updatebuild%]
+echo.  * If you want to try beta/pre-release builds of SysShivt tools,
+echo.    you might consider checking out https://github.com/Shivter14/sysshivt-tools
+echo.    (the download server) where you can even download in-dev canary builds.
+echo.  * Latest Dev/Pre-release build: %sst.latestdevbuild%
+echo.  * Latest Canary build: %sst.latestcanarybuild%
 if "%sst.updateargs%" neq "/silent" pause
 goto end
 :dev
