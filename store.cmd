b56772b522c00f1a30687fdda221577271d46de6
Shivter <98386496+Shivter14@users.noreply.github.com>
2024-06-18 16:26:01 +0200

    Update store.cmd

--- store.cmd
+++ store.cmd
@@ -1,43 +1,35 @@
-@echo off
-if "%sst.ver%" equ "" (
-	echo.This program requires SysShivt tools.
-	timeout 5 > nul
-	goto end
+@echo off & setlocal enabledelayedexpansion
+if not defined sst.ver (
+	echo.This program requires SysShivt tools. Press any key to exit. . .
+	pause>nul
+	exit /b
 )
 :start
 color f0
-mode 96,25
 cls
-echo.$debug=%sst.errorlevel%
-echo.  Welcome to SysShivt tools store %ls.username%.
-echo.  [BACKSPACE] / [ESC] = exit
-echo.
-echo.  Games:
-echo.  1 = FishGame
-echo.  2 = DontDownloadMe
+echo=$debug=!sst.errorlevel!
+echo=  Welcome to SysShivt tools store, !ls.username!.
+echo=  This store is an archive of old SysShivt tools games.
+echo=  Updated ^& archived by: Shivter
+echo=  [BACKSPACE] / [ESC] = exit
+echo=
+echo=  Games:
+echo=  1 = Fish Game
 getinput
-set sst.errorlevel=%errorlevel%
+set "sst.errorlevel=!errorlevel!"
 set errorlevel=
-if "%sst.errorlevel%" equ "8" goto end
-if "%sst.errorlevel%" equ "27" goto end
-if "%sst.errorlevel%" equ "49" goto installfishgame
-if "%sst.errorlevel%" equ "50" goto installvirus
+if "!sst.errorlevel!" equ "8" goto end
+if "!sst.errorlevel!" equ "27" goto end
+if "!sst.errorlevel!" equ "49" goto installfishgame
 goto start
 :installfishgame
+color 0f
+cls
 if not exist "..\downloads" md "..\downloads"
-cd ..\downloads
-call download.exe "https://github.com/Shivter14/SysShivt-tools/raw/main/fish.cmd" fishgame.cmd
-if not exist "getinput.exe" copy "%sst.dir%\getinput.exe" "getinput.exe"
+pushd ..\downloads
+curl -o fishgame.cmd "https://github.com/Shivter14/SysShivt-tools/raw/main/fish.cmd"
+if not exist "getinput.exe" copy "!sst.dir!\getinput.exe" "getinput.exe"
 cmd /c fishgame.cmd
-cd "%sst.dir%"
-goto start
-:installvirus
-if not exist "..\downloads" md "..\downloads"
-cd ..\downloads
-call download.exe "https://github.com/Shivter14/SysShivt-tools/raw/main/notavirus.cmd" notavirus.cmd
-if not exist "line.bat" copy "%sst.dir%\line.bat" "line.bat"
-if not exist "cmdwiz.exe" copy "%sst.dir%\cmdwiz.exe" "cmdwiz.exe"
-start /b cmd /c notavirus.cmd /b
-cd "%sst.dir%"
+popd
 goto start
 :end
